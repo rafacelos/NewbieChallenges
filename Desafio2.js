286738b85b9658117701cef3ad791e6015251606
Rafael Vasconcelos <rafa.celos.dev@gmail.com>
2020-02-22 00:34:13 +0000

    Finished Desafio2

--- Desafio2.js
+++ Desafio2.js
@@ -9,7 +9,18 @@ const empresa = {
         rua: "Rua Guilherme Gembala",
         numero: "260"
     }
-
 };
 
 console.log(`A empresa ${empresa.Nome} está localizada em ${empresa.Endereço.rua}, ${empresa.Endereço.numero}.`)
+
+const developer = {
+    nome: "Rafael",
+    idade: 25,
+    tecnologias: [
+        { nome: 'C++', especialidade: 'Desktop' },
+        { nome: 'Python', especialidade: 'Data Science' },
+        { nome: 'JavaScript', especialidade: 'Web/Mobile' }
+    ]
+};
+
+console.log(`O usuário ${developer.nome} tem ${developer.idade} anos e usa a tecnologia ${developer.tecnologias[0].nome} com especialidade em ${developer.tecnologias[0].especialidade}.`)
